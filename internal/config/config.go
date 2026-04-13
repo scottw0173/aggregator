9c94431edc31754354631ef1a6315a2a6f4b27d4
scottw0173 <swarner5937@gmail.com>
2026-04-13 00:23:53 -0700

    initial configuration writer

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,14 +1,63 @@
 package config
 
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+)
+
 type Config struct {
-	db_url    string
-	user_name string
+	DBURL    string `json:"db_url"`
+	UserName string `json:"user_name"`
 }
 
 func Read() (Config, error) {
-	return Config{}, nil
+	configPath, err := getCfgFilePath()
+	if err != nil {
+		return Config{}, err
+	}
+
+	data, err := os.ReadFile(configPath)
+	if err != nil {
+		return Config{}, err
+	}
+
+	var cfg Config
+	err = json.Unmarshal(data, &cfg)
+	if err != nil {
+		return Config{}, err
+	}
+
+	return cfg, nil
 }
 
-func (cfg Config) SetUser() error {
+func (cfg *Config) SetUser(name string) error {
+	cfg.UserName = name
+	configPath, err := getCfgFilePath()
+	if err != nil {
+		return err
+	}
+
+	data, err := json.Marshal(cfg)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(configPath, data, 0644)
+	if err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func getCfgFilePath() (string, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+
+	config_string := ".gatorconfig.json"
+	configPath := filepath.Join(home, config_string)
+	return configPath, nil
 }
--- main.go
+++ main.go
@@ -1,4 +1,22 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/scottw0173/aggregator/internal/config"
+)
+
 func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		fmt.Println("error creating config")
+	}
+	cfg.SetUser("KingOfAnts")
+
+	newCfg, err := config.Read()
+	if err != nil {
+		fmt.Println("error reading newCfg")
+	}
+
+	fmt.Println(newCfg)
 }
